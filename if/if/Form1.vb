dc5ebaddafcfeef28b72748836ea402023bb4bd3
Mbue <93818178+MbituM@users.noreply.github.com>
2021-12-14 23:19:30 +0300

    Add files via upload

---
+++ if/if/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Integer
+        num = txtnum.Text
+        If (num < 5) Then
+            MessageBox.Show("no room available")
+        ElseIf (num >= 5 And num <= 20) Then
+            MessageBox.Show("room A2")
+
+        ElseIf (num >= 21 And num <= 30) Then
+            MessageBox.Show("lab 4")
+
+        ElseIf (num >= 31 And num <= 60) Then
+            MessageBox.Show("Lab 1")
+
+        ElseIf (num >= 61 And num <= 100) Then
+            MessageBox.Show("Lab 3")
+        Else
+            MessageBox.Show("no room available")
+
+        End If
+
+    End Sub
+End Class
